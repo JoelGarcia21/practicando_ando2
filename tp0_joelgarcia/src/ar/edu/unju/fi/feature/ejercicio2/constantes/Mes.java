5a935dd972dc73da9c258c8164810e4e1e7bd54e
JoelGarcia21 <joel77270@gmail.com>
2024-04-26 11:19:01 -0300

    Paquetes y clases para el ejercicio 2 creados

---
+++ tp0_joelgarcia/src/ar/edu/unju/fi/feature/ejercicio2/constantes/Mes.java
@@ -0,0 +1,6 @@
+package ar.edu.unju.fi.feature.ejercicio2.constantes;
+
+public enum Mes {
+
+	ENERO, FEBRERO, MARZO, ABRIL, MAYO, JUNIO, JULIO, AGOSTO, SEPTIEMPRE, OCTUBRE, NOVIEMBRE, DICIEMBRE;
+}
